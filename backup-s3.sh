78edbb351cace7814bfc17c683788c7708a9154f
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-11-12 17:15:45 +0100

    Update backup-s3.sh

--- backup-s3.sh
+++ backup-s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# sudo mysqldump -u root --password="$db_pass"  kritika  tblEmployee1 > /tmp/dump3.sql
-# aws s3 cp  /tmp/dump3.sql s3://mysql-backup-s3-new
-sudo mkdir new-dir
+sudo mysqldump -u root --password="$db_pass"  kritika  tblEmployee1 > /tmp/dump3.sql
+aws s3 cp  /tmp/dump3.sql s3://mysql-backup-s3-new
+# sudo mkdir new-dir
